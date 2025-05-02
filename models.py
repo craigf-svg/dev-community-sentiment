151747e5ca1ce6cfae9619a9a20453e7ddb5b24e
craigf-svg <35483367+craigf-svg@users.noreply.github.com>
2025-05-01 21:53:03 -0400

    Update models.py

--- models.py
+++ models.py
@@ -15,9 +15,11 @@ class Post(db.Model):
     post_date_utc = db.Column(db.Date, nullable=False,
                               default=db.func.current_date())
     post_date_est = db.Column(db.Date, nullable=False)
-    sentiment_score = db.Column(db.SmallInteger)
-    model_label = db.Column(db.String(20))
-    model_confidence = db.Column(db.Float)
+    sentiment_label = db.Column(db.String(20)) # Positive, Negative, or Neutral
+    sentiment_score = db.Column(db.SmallInteger) # +1, -1, 0
+    model_label = db.Column(db.String(20)) # LABEL_0, LABEL_2, LABEL_1
+    model_confidence = db.Column(db.Float) # Range of [0-1.0] confidence in label
+
     created_at = db.Column(db.DateTime, nullable=False,
                            server_default=db.text("CURRENT_TIMESTAMP"))
     created_at_est = db.Column(db.Date, default=db.text(
@@ -33,6 +35,7 @@ class Post(db.Model):
             'timestamp_utc': self.timestamp_utc.strftime('%Y-%m-%d %H:%M:%S') if self.timestamp_utc else None,
             'post_date_utc': self.post_date_utc.strftime('%Y-%m-%d') if self.post_date_utc else None,
             'post_date_est': self.post_date_est.strftime('%Y-%m-%d') if self.post_date_est else None,
+            'sentiment_label': self.sentiment_label,
             'sentiment_score': self.sentiment_score,
             'model_label': self.model_label,
             'model_confidence': self.model_confidence
