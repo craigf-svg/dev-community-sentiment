f63e37dc168177e2c023805b2ab28595d8d39973
craigf-svg <35483367+craigf-svg@users.noreply.github.com>
2025-05-07 21:20:15 -0400

    insert post list

--- db_insert_posts.py
+++ db_insert_posts.py
@@ -2,20 +2,26 @@
 
 from models import db, Post
 
+SENTIMENT_SCORES = {
+    'LABEL_0': -1,
+    'LABEL_1': 0,
+    'LABEL_2': +1
+}
 
 def insert_post_list(post_list):
     inserted_count = 0
     for post in post_list:
         try:
+            # TODO: Make post_list follow format of ORM on creation
+            # Rather than mapping here for consistency
             int_timestamp_utc = int(post['timestamp_utc'])
-            print('int_timestamp_utc', int_timestamp_utc)
             new_post = Post(
                 # post_title=post['post_title'],
                 post_id=post['post_id'],
                 subreddit=post['subreddit'],
                 timestamp_utc=int_timestamp_utc,
                 sentiment_label=post['mood'],
-                sentiment_score=post['label_rank'],
+                sentiment_score=SENTIMENT_SCORES[post['label']],
                 model_label=post['label'],
                 model_confidence=post['score'],
             )
